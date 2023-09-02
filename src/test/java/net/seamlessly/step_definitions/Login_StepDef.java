8e048e0155d604efaea150fb547fead5b26344f4
Alec Velitskiy <alecvelitskiy@gmail.com>
2023-09-02 13:59:35 -0400

    I just created a step definition class and I did not implement my code yet.

---
+++ src/test/java/net/seamlessly/step_definitions/Login_StepDef.java
@@ -0,0 +1,16 @@
+package net.seamlessly.step_definitions;
+
+import io.cucumber.java.en.Given;
+
+public class Login_StepDef {
+
+    @Given("a")
+    public void a() {
+
+    }
+
+    @Given("b")
+    public void b() {
+
+    }
+}
