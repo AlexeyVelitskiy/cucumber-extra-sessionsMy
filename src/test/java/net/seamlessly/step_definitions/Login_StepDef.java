15ebb4a6ab8e0936b208ef3204ca283f7d0927b0
Alec Velitskiy <alecvelitskiy@gmail.com>
2023-09-02 14:52:50 -0400

    I just created step definition class and I did not implement my code yet.

--- src/test/java/net/seamlessly/step_definitions/Login_StepDef.java
+++ src/test/java/net/seamlessly/step_definitions/Login_StepDef.java
@@ -1,16 +1,22 @@
 package net.seamlessly.step_definitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 
 public class Login_StepDef {
 
-    @Given("a1")
-    public void a1() {
+    @Given("the user on the login page")
+    public void the_user_on_the_login_page() {
+
+    }
+    @Given("the user is logged in with valid credentials")
+    public void the_user_is_logged_in_with_valid_credentials() {
+
+    }
+    @Then("the user should see the url {string})")
+    public void the_user_should_see_the_url(String string){
 
     }
 
-    @Given("b1")
-    public void b1() {
-
-    }
 }
+
